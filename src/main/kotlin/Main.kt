fbcbdb6327e6043dca51f7730a295679036bb53c
Maureen-Gatweri <maureennmwendwa@gmail.com>
2024-03-25 08:33:47 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,47 @@
+fun main(){
+println(isPalindrome( ("madam, wow, kayak")))
+
+val records = arrayOf(4,5,7)
+   println(nums.sum)
+
+
+    call("Berning bakes down bagels and buns")
+
+
+
+}
+fun call(name:String){
+    records(nums = arrayOf(2,4,6,)))
+    println(nums.sum)
+
+
+   println(name)
+    println(name.indexOf("b"))
+    println(name.toCharArray(name))
+
+}
+
+fun records(nums:<array>Int):<arrayOf> {
+  return (nums.contentToString)
+
+
+
+}
+fun isPalindrome(word:String):Boolean{
+
+    if {
+     (word = "madam, wow, kayak")
+  return true
+  else
+  return false
+  }
+
+    }
+
+}
+   fun sphere(volume:Int){
+       val pi  = 3.14159
+    println( volume)
+   }
+
+
